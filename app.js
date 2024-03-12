9aafedd811f75bbc04e30167482fa8613f0a41b7
syedaffan592006 <syedaffan592006@gmail.com>
2024-03-11 18:35:31 -0700

    push

---
+++ app.js
@@ -0,0 +1,11 @@
+function getres(){
+var a =document.getElementById("math").Value;
+var b =document.getElementById("physics").Value;
+var c =document.getElementById("chemistry").Value;
+var d =document.getElementById("english").Value;
+var e =document.getElementById("urdu").Value;
+if(a > 100 ||b >100 ||)
+}
+
+
+
