055743749501ab1a11493ae622c3b5a00ccb7bf4
syedaffan592006 <syedaffan592006@gmail.com>
2024-03-11 19:23:32 -0700

    remainig

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-function getres(){
-var a =document.getElementById("math").Value;
-var b =document.getElementById("physics").Value;
-var c =document.getElementById("chemistry").Value;
-var d =document.getElementById("english").Value;
-var e =document.getElementById("urdu").Value;
-if(a > 100 ||b >100 ||)
+function getres() {
+  var a = document.getElementById("math").Value;
+  var b = document.getElementById("physics").Value;
+  var c = document.getElementById("chemistry").Value;
+  var d = document.getElementById("english").Value;
+  var e = document.getElementById("urdu").Value;
+  if (a > 100 || b > 100 || c > 100 || d > 100 || e > 100) {
+  } else {
+    var obtained = a + b + c + d + e;
+    document.getElementById("obt").innerHTML = obtained;
+    var per = (obtained / 500) * 100;
+    document.getElementById("per").innerHTML = per + "%";
+    if (per < 50) {
+      document.getElementById("grade").innerHTML = "F";
+    } else if (per > 50 && per < 60) {
+      document.getElementById("grade").innerHTML = "C";
+    } else if (per > 60 && per < 70) {
+      document.getElementById("grade").innerHTML = "B";
+    } else if (per > 70 && per < 100) {
+      document.getElementById("grade").innerHTML = "A";
+    }
+  }
 }
-
-
-
