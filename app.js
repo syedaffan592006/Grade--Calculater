f710f09c7a6b56f78b1e202e36860dab34324453
Syed Affan Asif <154745160+syedaffan592006@users.noreply.github.com>
2024-03-13 17:02:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function getres() {
-  var a = document.getElementById("math").Value;
-  var b = document.getElementById("physics").Value;
-  var c = document.getElementById("chemistry").Value;
-  var d = document.getElementById("english").Value;
-  var e = document.getElementById("urdu").Value;
-  if (a > 100 || b > 100 || c > 100 || d > 100 || e > 100) {
-  } else {
+function getres(){
+  var a = parseInt (document.getElementById("math").Value);
+  var b = parseInt (document.getElementById("physics").Value);
+  var c = parseInt (document.getElementById("chemistry").Value);
+  var d = parseInt (document.getElementById("english").Value);
+  var e = parseInt (document.getElementById("urdu").Value);
+  if (a > 100 || b > 100 || c > 100 || d > 100 || e > 100){
+alert("Enter correct number")
+  } 
+  else {
     var obtained = a + b + c + d + e;
     document.getElementById("obt").innerHTML = obtained;
-    var per = (obtained / 500) * 100;
+    var per = obtained / 500 * 100;
     document.getElementById("per").innerHTML = per + "%";
     if (per < 50) {
       document.getElementById("grade").innerHTML = "F";
